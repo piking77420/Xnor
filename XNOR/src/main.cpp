443ac061602cadc3ba726f4f8cc4cc9a64fa9487
Yohann DEMAISON--RÉGNÉ <yohann.demaison-regne@orange.fr>
2024-02-05 10:29:26 +0100

    Add glfw and imgui libs

--- XNOR/src/main.cpp
+++ XNOR/src/main.cpp
@@ -1,8 +1,68 @@
 #include <iostream>
+#include "GLFW/glfw3.h"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_opengl3.h"
+#include "imgui/imgui_impl_glfw.h"
 
 int main(int argc, char** argv)
 {
 	std::cout << "Hello, World!" << std::endl;
 
+	glfwInit();
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "XNOR", nullptr, nullptr);
+
+	glfwMakeContextCurrent(window);
+
+	glfwSwapInterval(1); // Enable vsync
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;     // Enable Docking
+	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;   // Enable viewports
+
+	io.Fonts->AddFontDefault();
+
+	// GL 3.0 + GLSL 130
+	const char* const glslVersion = "#version 430";
+
+	ImGui::StyleColorsDark();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(glslVersion);
+	
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		
+		// Start the Dear ImGui frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		ImGui::Begin("hello");
+		ImGui::End();
+
+		ImGui::Render();
+
+		int displayW, displayH;
+		glfwGetFramebufferSize(window, &displayW, &displayH);
+		glViewport(0, 0, displayW, displayH);
+		
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		GLFWwindow* ctxBackup = glfwGetCurrentContext();
+		ImGui::UpdatePlatformWindows();
+		ImGui::RenderPlatformWindowsDefault();
+		glfwMakeContextCurrent(ctxBackup);
+
+		glfwSwapBuffers(window);
+	}
+	
 	return 0;
 }
