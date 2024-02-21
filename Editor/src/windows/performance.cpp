2630a8247e2493c00b7cd568e45875b1e0226c8b
Maxime GRIMARD--HAMEL <m.grimardhamel@student.isartdigital.com>
2024-02-21 19:15:30 +0100

    Add performance window settings

--- Editor/src/windows/performance.cpp
+++ Editor/src/windows/performance.cpp
@@ -25,28 +25,26 @@ Performance::Performance(Editor* const editor, const size_t sampleSize)
 
 void Performance::Update()
 {
-    if (Calc::OnInterval(XnorCore::Time::GetTotalTime(), XnorCore::Time::GetLastTotalTime(), UpdateInterval))
-    {
-        m_LastFps = 1.f / XnorCore::Time::GetDeltaTime();
-        m_FrameRateArray[m_ArrayIndex] = m_LastFps;
-        m_HighestArrayFps = std::max(*std::ranges::max_element(m_FrameRateArray), MinHighestArrayFps);
+    m_LastFps = 1.f / XnorCore::Time::GetDeltaTime();
+    m_FrameRateArray[m_ArrayIndex] = m_LastFps;
+    m_HighestArrayFps = std::max(*std::ranges::max_element(m_FrameRateArray), MinHighestArrayFps);
 
-        PROCESS_MEMORY_COUNTERS memory;
-        GetProcessMemoryInfo(GetCurrentProcess(), &memory, sizeof(memory));
-        
-        m_LastMemory = static_cast<float_t>(memory.WorkingSetSize) / 0x100000;
-        m_MemoryArray[m_ArrayIndex] = m_LastMemory;
-        m_HighestArrayMemory = std::max(*std::ranges::max_element(m_MemoryArray), MinHighestArrayMemory);
+    PROCESS_MEMORY_COUNTERS memory;
+    GetProcessMemoryInfo(GetCurrentProcess(), &memory, sizeof(memory));
+    
+    m_LastMemory = static_cast<float_t>(memory.WorkingSetSize) / 0x100000;
+    m_MemoryArray[m_ArrayIndex] = m_LastMemory;
+    m_HighestArrayMemory = std::max(*std::ranges::max_element(m_MemoryArray), MinHighestArrayMemory);
 
-        m_ArrayIndex = static_cast<int32_t>((m_ArrayIndex + 1) % m_FrameRateArray.size());
-        if (m_TotalSamples < MaxSamples)
-            m_TotalSamples++;
-    }
+    m_ArrayIndex = static_cast<int32_t>((m_ArrayIndex + 1) % m_FrameRateArray.size());
+    if (m_TotalSamples < MaxSamples)
+        m_TotalSamples++;
 }
 
 void Performance::Display()
 {
-    Update();
+    if (Calc::OnInterval(XnorCore::Time::GetTotalTime(), XnorCore::Time::GetLastTotalTime(), m_UpdateInterval))
+        Update();
     
     const ImVec2 available = ImGui::GetContentRegionAvail();
     std::string format = std::format("FPS: {:.0f}", m_LastFps);
@@ -58,4 +56,7 @@ void Performance::Display()
     format = std::format("Memory: {:.2f}MB", m_LastMemory);
     ImGui::PlotLines("##memory", m_MemoryArray.data(), static_cast<int32_t>(std::min(m_TotalSamples, m_MemoryArray.size())), m_ArrayIndex,
         format.c_str(), 0.f, m_HighestArrayMemory, ImVec2(available.x, GraphsHeight));
+
+    if (ImGui::CollapsingHeader("Settings"))
+        ImGui::SliderFloat("Update rate", &m_UpdateInterval, 0.f, 2.f);
 }
