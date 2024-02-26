bb596438958f65e182c32f3f075e9583cf1562f6
Gabriel BON <g.bon@student.isartdigital.com>
2024-02-26 13:19:42 +0100

    Add moving one right and up camera axis on press down the middle mous button

--- Editor/include/editor_camera.hpp
+++ Editor/include/editor_camera.hpp
@@ -14,20 +14,30 @@ public:
     void UpdateCamera(XnorCore::Camera* camera);
 
 private:
-    void ResetCameraRotation();
+    void ResetDeltatMouse();
+
+    void CameraOnRightClick(XnorCore::Camera* camera);
     
     void EditorCameraRotation(XnorCore::Camera* camera);
 
     void EditorCameraMovement(XnorCore::Camera* camera);
+
+    void OnMiddleButton(XnorCore::Camera* camera);
+
+    void ComputeDeltaMouse();
     
     bool m_FirstMove = false;
     float_t m_MouseSensitivity = 0.1f;
     float_t m_Yaw = 90.f;
     float_t m_Pitch = 0.f;
-    float_t m_LastX = 0.f;
-    float_t m_LastY = 0.f;
+    Vector2 m_LastInput;
     bool m_IsMoving = false;
     float_t m_CameraSpeed = 2.5f;
+
+    bool m_ComputeDeltaMouse = false;
+    bool m_ResetDeltaMouse = false;
+
+    Vector2 m_MouseOffSet;
     
 };
 
--- Editor/src/editor_camera.cpp
+++ Editor/src/editor_camera.cpp
@@ -9,48 +9,58 @@ void EditorCamera::UpdateCamera(XnorCore::Camera* camera)
 {
     if (!camera)
         return;
+
+    CameraOnRightClick(camera);
+    OnMiddleButton(camera);
     
-    if (XnorCore::Input::GetMouseButton(XnorCore::MouseButton::Right, XnorCore::MouseButtonStatus::Down) && m_IsMoving == false)
-        m_IsMoving = true;
+    if(m_ComputeDeltaMouse)
+        ComputeDeltaMouse();
 
-    if (XnorCore::Input::GetMouseButton(XnorCore::MouseButton::Right, XnorCore::MouseButtonStatus::Release))
-        ResetCameraRotation();
-
-    if (m_IsMoving)
-        EditorCameraRotation(camera);
+    if(m_ResetDeltaMouse)
+        ResetDeltatMouse();
+    
         
     EditorCameraMovement(camera);
+    OnMiddleButton(camera);
 }
     
 
-void EditorCamera::ResetCameraRotation()
+void EditorCamera::ResetDeltatMouse()
 {
     m_FirstMove = false;
-    m_IsMoving = false;
-    m_LastX = 0;
-    m_LastY = 0;
+    m_LastInput = Vector2(0.f);
+}
+
+void EditorCamera::CameraOnRightClick(XnorCore::Camera* camera)
+{
+    if (XnorCore::Input::GetMouseButton(XnorCore::MouseButton::Right, XnorCore::MouseButtonStatus::Release))
+    {
+        m_ResetDeltaMouse = true;
+    }
+    else
+    {
+        m_ResetDeltaMouse = false;
+    }
+    
+    if (XnorCore::Input::GetMouseButton(XnorCore::MouseButton::Right, XnorCore::MouseButtonStatus::Down))
+    {
+        m_ComputeDeltaMouse = true;
+        EditorCameraRotation(camera);
+    }
+    else
+    {
+        m_ComputeDeltaMouse = false;
+    }
 }
 
 void EditorCamera::EditorCameraRotation(XnorCore::Camera* camera)
 {
     using namespace XnorCore;
+
+    ComputeDeltaMouse();
     
-    const Vector2 mousePos = Input::GetCursorPos<Vector2>();
-    
-    if (!m_FirstMove)
-    {
-        m_LastX = mousePos.x;
-        m_LastY = mousePos.y;
-        m_FirstMove = true;
-    } 
-    
-    const float xoffset = mousePos.x - m_LastX ;
-    const float yoffset = m_LastY - mousePos.y ; // reversed since y-coordinates range from bottom to top
-    m_LastX = mousePos.x;
-    m_LastY = mousePos.y;
-    
-    m_Yaw   += xoffset;
-    m_Pitch += yoffset; 
+    m_Yaw   += m_MouseOffSet.x;
+    m_Pitch += m_MouseOffSet.y; 
     
     if(m_Pitch > MaxPitch)
         m_Pitch = MaxPitch;
@@ -89,6 +99,51 @@ void EditorCamera::EditorCameraMovement(XnorCore::Camera* camera)
 
     if (Input::GetKey(Key::LeftControl, KeyStatus::Down))
         camera->pos -= Vector3::UnitY() * cameraSpeed;
+}
+
+void EditorCamera::OnMiddleButton(XnorCore::Camera* camera)
+{
+    using namespace XnorCore;
+    
+    if (Input::GetMouseButton(MouseButton::Middle,MouseButtonStatus::Down))
+    {
+        m_ComputeDeltaMouse = true;
+        const Vector3 vector = (camera->right * -m_MouseOffSet.x) + (camera->up * m_MouseOffSet.y);
+        camera->pos += vector * Time::GetDeltaTime() * m_CameraSpeed ; 
+    }
+    else
+    {
+        m_ComputeDeltaMouse = false;
+    }
+    
+    if (Input::GetMouseButton(MouseButton::Middle,MouseButtonStatus::Release))
+    {
+        m_ResetDeltaMouse = true;
+    }
+    else
+    {
+        m_ResetDeltaMouse = false;
+    }
+    
+}
+
+void EditorCamera::ComputeDeltaMouse()
+{
+    using namespace XnorCore;
+
+    const Vector2 mousePos = Input::GetCursorPos<Vector2>();
+    
+    if (!m_FirstMove)
+    {
+        m_LastInput.x = mousePos.x;
+        m_LastInput.y = mousePos.y;
+        m_FirstMove = true;
+    } 
+    
+    m_MouseOffSet.x = mousePos.x - m_LastInput.x ;
+    m_MouseOffSet.y = m_LastInput.y - mousePos.y ; // reversed since y-coordinates range from bottom to top
+    m_LastInput.x = mousePos.x;
+    m_LastInput.y = mousePos.y;
 }
 
 
