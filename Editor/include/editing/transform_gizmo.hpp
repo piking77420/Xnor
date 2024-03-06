59f4f5049f533c6795d7a0ed410503c63a910e4c
Gabriel BON <g.bon@student.isartdigital.com>
2024-03-06 15:54:50 +0100

    Add editor transform gizmo

---
+++ Editor/include/editing/transform_gizmo.hpp
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+
+#include "core.hpp"
+#include "definitions.hpp"
+#include "Maths/matrix.hpp"
+
+#include <ImGui/imgui.h>
+#include<ImguiGizmo/ImGuizmo.h>
+
+#include "transform.hpp"
+#include "rendering/camera.hpp"
+
+BEGIN_XNOR_EDITOR
+class TransfromGizmo
+{
+public:
+
+    DEFAULT_COPY_MOVE_OPERATIONS(TransfromGizmo)
+
+    TransfromGizmo() = default;
+
+    ~TransfromGizmo() = default;
+
+    void SetRendering(const XnorCore::Camera& camera, Vector2 windowPos, Vector2 windowSize);
+
+    bool Manipulate(XnorCore::Transform& transform);
+private:
+    ImGuizmo::OPERATION m_CurrentGizmoOperation = ImGuizmo::TRANSLATE;
+    ImGuizmo::MODE m_CurrentGizmoMode = ImGuizmo::WORLD;
+    
+    Matrix m_View;
+    Matrix m_Projection;
+    
+};
+
+END_XNOR_EDITOR
