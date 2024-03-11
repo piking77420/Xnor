820178c29f6050ef5387ffa6e094fef2498acb79
Gabriel BON <g.bon@student.isartdigital.com>
2024-03-11 14:57:05 +0100

    Remove log

--- Editor/src/windows/editor_window.cpp
+++ Editor/src/windows/editor_window.cpp
@@ -100,7 +100,6 @@ void EditorWindow::SelectEntityOnScreen()
         XnorCore::Entity* ptr = nullptr;
         if (m_PickingStrategy.GetEntityFromScreen(mouseposI, XnorCore::World::scene, m_Editor->data.editorCam, &ptr))
         {
-            XnorCore::Logger::LogInfo("Entity name is {}", ptr->name);
             m_Editor->data.selectedEntity = ptr;
             return;
         }
