ffa3e72d22875b83ea29a632e931aa424fbd6b1f
y.demaisonregne <y.demaisonregne@student.isartdigital.com>
2024-03-15 08:50:58 +0100

    Split event class with inline file

--- Core/include/utils/event.hpp
+++ Core/include/utils/event.hpp
@@ -45,40 +45,6 @@ private:
     std::vector<std::function<FunctionT>> m_Functions;
 };
 
-template <typename... Args>
-void Event<Args...>::Invoke(Args... args) const
-{
-    for (const std::function<FunctionT>& function : m_Functions)
-        function(std::forward<Args>(args)...);
-}
-
-template <typename... Args>
-void Event<Args...>::Clear()
-{
-    m_Functions.clear();
-}
-
-template <typename... Args>
-Event<Args...>& Event<Args...>::operator+=(std::function<FunctionT> func)
-{
-    m_Functions.push_back(std::move(func));
-
-    return *this;
-}
-
-template <typename ... Args>
-Event<Args...>& Event<Args...>::operator-=(const std::function<FunctionT>& func)
-{
-    for (typename decltype(m_Functions)::const_iterator it = m_Functions.begin(); it != m_Functions.end(); it++)
-    {
-        if (Utils::FunctionAddress(*it) == Utils::FunctionAddress(func))
-        {
-            m_Functions.erase(it);
-            break;
-        }
-    }
-
-    return *this;
-}
-
 END_XNOR_CORE
+
+#include "utils/event.inl"
