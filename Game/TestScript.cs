8e36fb5df002884f1365bca3c56232bbf5fb7523
Maxime GRIMARD--HAMEL <m.grimardhamel@student.isartdigital.com>
2024-03-25 10:00:59 +0100

    Fix C# assembly reloadin

--- Game/TestScript.cs
+++ Game/TestScript.cs
@@ -29,6 +29,7 @@ namespace Game
             color.h++;
 
             light.color = (Colorf) color;
+            light.intensity = 200f;
 
             transform.SetPositionX(MathF.Cos(Time.GetTotalTime()) * 2f);
             transform.SetPositionZ(MathF.Sin(Time.GetTotalTime()) * 2f);
