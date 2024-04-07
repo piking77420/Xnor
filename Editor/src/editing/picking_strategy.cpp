1260b266c1a625abecc3bfb01c72637b9681a336
y.demaisonregne <y.demaisonregne@student.isartdigital.com>
2024-04-07 18:38:08 +0200

    Fix picking strategy

--- Editor/src/editing/picking_strategy.cpp
+++ Editor/src/editing/picking_strategy.cpp
@@ -5,14 +5,14 @@
 
 using namespace XnorEditor;
 
-PickingStrategy::PickingStrategy(Editor* editor) : m_Editor(editor)
+PickingStrategy::PickingStrategy(Editor* editor)
+    : m_Editor(editor)
 {
     const Vector2i windowSize = XnorCore::Window::GetSize();
     InitRendering(windowSize);
     
     m_PickingShader = XnorCore::ResourceManager::Get<XnorCore::Shader>("picking_shader");
     m_PickingShader->CreateInRhi();
-    
 }
 
 PickingStrategy::~PickingStrategy()
@@ -20,21 +20,17 @@ PickingStrategy::~PickingStrategy()
     DestroyRendering();
 }
 
-void PickingStrategy::ResizeHandle(Vector2i newSize)
+void PickingStrategy::ResizeHandle(const Vector2i newSize)
 {
-    if(m_ColorAttachment->GetSize() != newSize)
+    if (m_ColorAttachment->GetSize() != newSize)
     {
         DestroyRendering();
         InitRendering(newSize);
     }
 }
 
-bool PickingStrategy::GetEntityFromScreen(
-    Vector2i pixelPos,
-    XnorCore::Scene& scene,
-    const XnorCore::Camera& pointOfView,
-    XnorCore::Entity** entity
-)
+bool_t PickingStrategy::GetEntityFromScreen(const Vector2i pixelPos, XnorCore::Scene& scene, const XnorCore::Camera& pointOfView,
+    XnorCore::Entity** entity)
 {
     if (!m_Editor)
         return false;
@@ -44,42 +40,37 @@ bool PickingStrategy::GetEntityFromScreen(
 
     // Draw
     m_PickingShader->Use();
-    XnorCore::RenderPassBeginInfo beginInfo =
+    const XnorCore::RenderPassBeginInfo beginInfo =
     {
         .frameBuffer = frameBuffer,
         .renderAreaOffset = { 0, 0 },
         .renderAreaExtent = m_ColorAttachment->GetSize(),
         .clearBufferFlags = static_cast<XnorCore::BufferFlag::BufferFlag>(XnorCore::BufferFlag::ColorBit | XnorCore::BufferFlag::DepthBit),
-        .clearColor = Vector4(0.f)
+        .clearColor = Vector4::Zero()
     };
-    m_Editor->renderer.RenderNonShaded(pointOfView,beginInfo,m_ColorPass,m_PickingShader,scene,true);
+    
+    m_Editor->renderer.RenderNonShaded(pointOfView, beginInfo, m_ColorPass, m_PickingShader, scene, true);
     m_PickingShader->Unuse();
 
-    
     float_t getValue = 0.f;
-    frameBuffer->GetPixelFromAttachment(0,pixelPos, XnorCore::TextureFormat::Red, XnorCore::DataType::Float,&getValue);
+    frameBuffer->GetPixelFromAttachment(0, pixelPos, XnorCore::TextureFormat::Red, XnorCore::DataType::Float, &getValue);
 
-    if(getValue == 0.f)
+    if (getValue == 0.f)
         return false;
 
-    // - 1 cause in render wee need to do +1 to avoid the black color of the attachement be a valid index  
-    const uint32_t EntityIndex = static_cast<uint32_t>(getValue) - 1;
+    // - 1 cause in render wee need to do +1 to avoid the black color of the attachment be a valid index  
+    const uint32_t entityIndex = static_cast<uint32_t>(getValue) - 1;
 
     const XnorCore::List<XnorCore::Entity*>& entities = scene.GetEntities();
-    
-    for (size_t i = 0 ; i < entities.GetSize(); i++)
+
+    if (entityIndex < entities.GetSize())
     {
-        if(EntityIndex == i)
-        {
-            *entity = entities[i];
-            return true;
-        }
+        *entity = entities[entityIndex];
+        return true;
     }
     
     return false;
 }
-
-
 
 void PickingStrategy::DestroyRendering() const
 {
@@ -91,8 +82,8 @@ void PickingStrategy::DestroyRendering() const
 void PickingStrategy::InitRendering(const Vector2i size)
 {
     frameBuffer = new XnorCore::Framebuffer();
-    m_ColorAttachment = new XnorCore::Texture(XnorCore::TextureInternalFormat::R32F,size);
-    m_DepthAttachement = new XnorCore::Texture(XnorCore::TextureInternalFormat::DepthComponent16,size);
+    m_ColorAttachment = new XnorCore::Texture(XnorCore::TextureInternalFormat::R32F, size);
+    m_DepthAttachement = new XnorCore::Texture(XnorCore::TextureInternalFormat::DepthComponent16, size);
 
     const std::vector<XnorCore::RenderTargetInfo> attachementsType =
     {
@@ -106,6 +97,5 @@ void PickingStrategy::InitRendering(const Vector2i size)
 
     m_ColorPass = XnorCore::RenderPass(attachementsType);
     const std::vector<const XnorCore::Texture*> targets = { m_ColorAttachment, m_DepthAttachement };
-    frameBuffer->AttachTextures(m_ColorPass,targets);
-
+    frameBuffer->AttachTextures(m_ColorPass, targets);
 }
